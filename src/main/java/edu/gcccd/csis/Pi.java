ca205bdd581d6e4d065816202960055941fef705
onel-shina <onelraad@gmail.com>
2019-10-11 03:04:36 -0700

    second commit

--- src/main/java/edu/gcccd/csis/Pi.java
+++ src/main/java/edu/gcccd/csis/Pi.java
@@ -6,9 +6,9 @@ public class Pi {
         System.out.println(calculatePi());
     }
 
-    public static double calculatePi() {
-        int c = 0;
-        int s = 0;
+    protected static double calculatePi() {
+        double c = 0; //type double, so I don't have to cast later when dividing
+        double s = 0; //type double, so I don't have to cast later when dividing
         double point;
         double estimatedPi;
         double actualPi = Math.PI;
@@ -21,10 +21,10 @@ public class Pi {
             double y = random.nextDouble(); // generate random y value //
             s++; // increment number of points inside the square //
             point = (x * x) + (y * y); // Pythagorean theorem //
-            if (point <= 1) { // If point is less than or equal to 1, point lies inside the circle //
+            if (point <= 1) { //Pythagorean theorem, If point is less than or equal to 1, point lies inside the circle //
                 c++;// increment number of points inside the circle //
             }
-            estimatedPi = 4 * ((double) c / (double) s); // estimating Pi value //
+            estimatedPi = 4 * (c / s); // estimating Pi value //
             error = Math.abs(actualPi - estimatedPi); //calculating error between estimatedPi and actualPi //
             if (error <= allowedError) { //if error is less than 0.00000001, break. //
                 break;
